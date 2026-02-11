44b1deb7cd18d017a2c1c18a1cb143403d2ada0e
llandauro <lara.landauro@gmail.com>
2026-02-10 19:55:14 -0500

    matchinglogic.ts member definition

---
+++ src/matchingLogic.ts
@@ -0,0 +1,10 @@
+// Define ALPFA member that would be a mentee or mentor
+export interface Member {
+    name: string;
+    experience: number; // in years
+    field: string;
+    careerStage: string; // e.g., "early", "mid", "late"
+    studies : string; // e.g., "Computer Science", "Business", etc.
+    objectives: string; // e.g., "career growth", "networking", etc.
+}
+
